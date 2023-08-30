0bef293bee4be63010f784974e09e09b3240aeb8
Layal <layal.haidar136@gmail.com>
2023-08-30 13:40:20 +0300

    expense tracker

---
+++ lib/expenses.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  _ExpensesState createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
