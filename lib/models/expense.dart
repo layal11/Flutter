77d2891851d178c3ca0bd83a905d4fd02868de48
Layal <layal.haidar136@gmail.com>
2023-09-14 11:23:20 +0300

    add constructor function and filter list

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -36,3 +36,29 @@ class Expense {
     return myDateFormatter.format(date);
   }
 }
+
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  // named constructor having some logic
+  //filter out the List of expenses that belongs to a specific category (this.category)
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Categories category;
+  final List<Expense> expenses;
+
+  double get getTotalExpenses {
+    double sum = 0;
+
+    for (var expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
+  }
+}
