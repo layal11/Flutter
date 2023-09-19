2aa4acedb84634047cb459e83b80ce50edf043ad
Layal <layal.haidar136@gmail.com>
2023-09-19 20:33:37 +0300

    add visual tapping effect fo inkwell

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -9,22 +9,35 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        color: category.color,
+    return Material(
+      color: category.color,
+      borderRadius: const BorderRadius.all(
+        Radius.circular(20),
+      ),
+      child: InkWell(
+        onTap: () {},
+        splashColor: Theme.of(context).primaryColorDark, //visual tapping effect
         borderRadius: const BorderRadius.all(
           Radius.circular(20),
         ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context)
-            .textTheme
-            .titleLarge!
-            .copyWith(color: Theme.of(context).colorScheme.onBackground),
-        //or we can style it just like this
-        //style: Theme.of(context).textTheme.titleLarge,
+
+        child: Container(
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(
+              Radius.circular(20),
+            ),
+          ),
+          child: Text(
+            category.title,
+            style: Theme.of(context)
+                .textTheme
+                .titleLarge!
+                .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            //or we can style it just like this
+            //style: Theme.of(context).textTheme.titleLarge,
+          ),
+        ),
       ),
     );
   }
