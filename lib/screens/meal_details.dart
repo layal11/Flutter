8b99cc16fa0f021dcd8f8035fac308c453f5361a
Layal <layal.haidar136@gmail.com>
2023-10-03 15:55:46 +0300

    display all data to the single meal

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -1,5 +1,6 @@
 import 'package:first_flutter_app/models/meal.dart';
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class MealDetailsScreen extends StatelessWidget {
   const MealDetailsScreen({super.key, required this.meal});
@@ -13,15 +14,67 @@ class MealDetailsScreen extends StatelessWidget {
         title: Text(meal.title),
         actions: const [],
       ),
-      body: Image.network(
-        meal.imageUrl,
-        fit: BoxFit.cover,
-        width: double.infinity,
+      body: ListView(
+        children: [
+          FadeInImage(
+            placeholder: MemoryImage(kTransparentImage),
+            image: NetworkImage(
+              meal.imageUrl,
+            ),
+            fit: BoxFit.cover,
+            height: 280,
+            width: double.infinity,
+          ),
+          const SizedBox(height: 20),
+          Text(
+            'Ingredients',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.w600,
+                ),
+          ),
+          const SizedBox(height: 15),
+          for (final ingredient in meal.ingredients)
+            Column(
+              children: [
+                Text(
+                  ingredient,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color:
+                          Theme.of(context).colorScheme.onSecondaryContainer),
+                ),
+              ],
+            ),
+          const SizedBox(height: 20),
+          Text(
+            'Steps',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.w600,
+                ),
+          ),
+          const SizedBox(height: 15),
+          for (final allSteps in meal.steps)
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '- $allSteps',
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                        color:
+                            Theme.of(context).colorScheme.onSecondaryContainer),
+                  ),
+                ],
+              ),
+            ),
+          const SizedBox(height: 30),
+        ],
       ),
     );
-
-    //     Text('affordability'),
-    //     Text('complexity'),
-    //     Text('ingredients'),
   }
 }
