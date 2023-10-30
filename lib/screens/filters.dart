1571578b4afd8568afc7625ab32017cd96d99a21
Layal <layal.haidar136@gmail.com>
2023-10-30 14:26:52 +0200

    outsource state from the provider and change FiltersScreen from StatefulWidget to StatelessWidget

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -4,34 +4,13 @@ import 'package:first_flutter_app/providers/filters_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FiltersScreen extends ConsumerStatefulWidget {
+class FiltersScreen extends ConsumerWidget {
   const FiltersScreen({super.key});
 
   @override
-  ConsumerState<FiltersScreen> createState() => _FiltersState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final fltrProviderData = ref.watch(fltrsProvider);
 
-class _FiltersState extends ConsumerState<FiltersScreen> {
-  bool toggleGlutenFree = false;
-  bool toggleIsVegan = false;
-  bool toggleIsVegetarian = false;
-  bool toggleIsLactoseFree = false;
-
-  @override
-  void initState() {
-    //initState runs before the build method executes
-    super.initState();
-
-    final providerFilterData = ref.read(fltrsProvider);
-
-    toggleGlutenFree = providerFilterData[Filter.glutenFree]!;
-    toggleIsVegan = providerFilterData[Filter.vegan]!;
-    toggleIsVegetarian = providerFilterData[Filter.vegetarian]!;
-    toggleIsLactoseFree = providerFilterData[Filter.lactoseFree]!;
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Filters'),
@@ -48,98 +27,87 @@ class _FiltersState extends ConsumerState<FiltersScreen> {
       //     }
       //   },
       // ),
-      body: WillPopScope(
-        onWillPop: () async {
-          ref.read(fltrsProvider.notifier).setFilters({
-            Filter.glutenFree: toggleGlutenFree,
-            Filter.lactoseFree: toggleIsLactoseFree,
-            Filter.vegan: toggleIsVegan,
-            Filter.vegetarian: toggleIsVegetarian,
-          });
-          return true; //true to allow flutter to pop/close
-        },
-        child: Column(
-          children: [
-            SwitchListTile(
-              value: toggleGlutenFree,
-              onChanged: (value) => setState(() {
-                toggleGlutenFree = value;
-              }),
-              activeColor: Theme.of(context).colorScheme.tertiary,
-              contentPadding: const EdgeInsets.only(left: 17, right: 17),
-              title: Text(
-                'Gluten-free',
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground,
-                    ),
-              ),
-              subtitle: Text(
-                'Only includes gluten-free meals.',
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground,
-                    ),
-              ),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: fltrProviderData[Filter.glutenFree]!,
+            onChanged: (valueChecked) => ref
+                .read(fltrsProvider.notifier)
+                .setFilter(Filter.glutenFree, valueChecked),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 17, right: 17),
+            title: Text(
+              'Gluten-free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
             ),
-            SwitchListTile(
-              value: toggleIsVegan,
-              onChanged: (value) => setState(() {
-                toggleIsVegan = value;
-              }),
-              activeColor: Theme.of(context).colorScheme.tertiary,
-              contentPadding: const EdgeInsets.only(left: 17, right: 17),
-              title: Text(
-                'Vegan',
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground,
-                    ),
-              ),
-              subtitle: Text(
-                'Only includes vegan meals.',
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground,
-                    ),
-              ),
+            subtitle: Text(
+              'Only includes gluten-free meals.',
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
             ),
-            SwitchListTile(
-              value: toggleIsVegetarian,
-              onChanged: (value) => setState(() {
-                toggleIsVegetarian = value;
-              }),
-              activeColor: Theme.of(context).colorScheme.tertiary,
-              contentPadding: const EdgeInsets.only(left: 17, right: 17),
-              title: Text(
-                'Vegetarian',
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground,
-                    ),
-              ),
-              subtitle: Text(
-                'Only includes vegetarian meals.',
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground,
-                    ),
-              ),
+          ),
+          SwitchListTile(
+            value: fltrProviderData[Filter.vegan]!,
+            onChanged: (valueChecked) => ref
+                .read(fltrsProvider.notifier)
+                .setFilter(Filter.vegan, valueChecked),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 17, right: 17),
+            title: Text(
+              'Vegan',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
             ),
-            SwitchListTile(
-              value: toggleIsLactoseFree,
-              onChanged: (value) => setState(() {
-                toggleIsLactoseFree = value;
-              }),
-              title: Text(
-                'Lactose-free',
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground,
-                    ),
-              ),
-              subtitle: Text(
-                'Only includes Lactose-free meals.',
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: Theme.of(context).colorScheme.onBackground,
-                    ),
-              ),
-            )
-          ],
-        ),
+            subtitle: Text(
+              'Only includes vegan meals.',
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+          ),
+          SwitchListTile(
+            value: fltrProviderData[Filter.vegetarian]!,
+            onChanged: (valueChecked) => ref
+                .read(fltrsProvider.notifier)
+                .setFilter(Filter.vegetarian, valueChecked),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 17, right: 17),
+            title: Text(
+              'Vegetarian',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Only includes vegetarian meals.',
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+          ),
+          SwitchListTile(
+            value: fltrProviderData[Filter.lactoseFree]!,
+            onChanged: (valueChecked) => ref
+                .read(fltrsProvider.notifier)
+                .setFilter(Filter.lactoseFree, valueChecked),
+            title: Text(
+              'Lactose-free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Only includes Lactose-free meals.',
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+          )
+        ],
       ),
     );
   }
