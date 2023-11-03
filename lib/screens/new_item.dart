547bbf10e77093c2b3312bc543c4acc41645c18c
Layal <layal.haidar136@gmail.com>
2023-11-03 14:49:00 +0200

    add NewItem screen Form

--- lib/screens/new_item.dart
+++ lib/screens/new_item.dart
@@ -1,3 +1,4 @@
+import 'package:first_flutter_app/data/categories.dart';
 import 'package:flutter/material.dart';
 
 class NewItem extends StatefulWidget {
@@ -16,9 +17,94 @@ class _NewItemState extends State<NewItem> {
       appBar: AppBar(
         title: const Text(appBarTitle),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(20),
-        child: Text('the from'),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    validator: (value) {
+                      return 'Demo...';
+                    },
+                    maxLength: 50,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      label: const Text('Name'),
+                      labelStyle: TextStyle(
+                        color: (Colors.amber[200]),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          validator: (value) {
+                            return 'Demo..';
+                          },
+                          initialValue: '1',
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            label: const Text('Quantity'),
+                            labelStyle: TextStyle(
+                              color: (Colors.amber[200]),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        child: DropdownButtonFormField(
+                          onChanged: (value) {},
+                          items: categories.entries
+                              .map(
+                                (category) => DropdownMenuItem(
+                                  value: category.value,
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        width: 20,
+                                        height: 20,
+                                        color: category.value.color,
+                                      ),
+                                      const SizedBox(width: 10),
+                                      Text(category.value.name),
+                                    ],
+                                  ),
+                                ),
+                              )
+                              .toList(),
+
+                          // items: [
+                          //   for (final category in categories.entries)
+                          //     DropdownMenuItem(
+                          //       child: Row(
+                          //         children: [
+                          //           Expanded(
+                          //             child: Container(
+                          //               width: 20,
+                          //               height: 20,
+                          //               color: category.value.color,
+                          //             ),
+                          //           ),
+                          //           const SizedBox(width: 10),
+                          //           Text(category.value.name),
+                          //         ],
+                          //       ),
+                          //     ),
+                          // ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
